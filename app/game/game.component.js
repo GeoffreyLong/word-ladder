f750dfb6c4bd2ca6baf16c7d33ed99f319cd7461
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-08-11 14:01:28 -0400

    REFACTOR simplify logic for first game type
    This game will undoubtedly be the hardest. Every letter has to end at the last letter placed. If you are going a direction you can only continue in that direction. This means that all words will start/end at the last letter placed. This will eliminate overlap cases.

--- app/game/game.component.js
+++ app/game/game.component.js
@@ -11,71 +11,68 @@ angular.module('game').component('game', {
     //    TODO describe
     // TODO should reversed words count?
     //      perhaps this could be an "easy mode"
-    // ALTERNATE
-    //    Have the index / offset as the top level
-    //        i.e. 1: {-3: 'a', -2: 't'}
-    //        Then can access like gameLetters[1][-3] I think
-    //        Also I think arrays in javascript can have negative indices
-    //            So could just do that...
-    //        Then checking adjacent letters would be pretty easy
-    $scope.gameLetters = {
-      orientation: 'vert',
-      enabled: [{
+    // NOTE For this version I am doing non-overlapping letters for vertical
+    //      When you start a new vertical word, it has it's last letter on old
+    //      The current selected is always the last letter placed
+    //      And once you have selected a direction (up, left, right)
+    //        you can only add letters in that direction
+    // ALTR I can do non-overlapping for vertical and overlapping for horiz
+    //      This means that I have to add a cur selected
+    $scope.game = {
+      direction: 'vert',
+      gameLetters: [{
         letter: 'r',
-        offsetX: 1,
-        offsetY: 5
+        offsetX: 3,
+        offsetY: 7
+      }, {
+        letter: 'e',
+        offsetX: 3,
+        offsetY: 6
       }, {
         letter: 'n',
+        offsetX: 3,
+        offsetY: 5
+      }, {
+        letter: 'd',
+        offsetX: 3,
+        offsetY: 4
+      }, {
+        letter: 'n',
+        offsetX: 2,
+        offsetY: 4
+      }, {
+        letter: 'e',
         offsetX: 1,
-        offsetY: 3
+        offsetY: 4
+      }, {
+        letter: 's',
+        offsetX: 0,
+        offsetY: 4
       }, {
         letter: 't',
-        offsetX: 1,
+        offsetX: 0,
+        offsetY: 3
+      }, {
+        letter: 'a',
+        offsetX: 0,
         offsetY: 2
-      }],
-      disabled: [
-        [{
-          letter: 'e',
-          offsetX: 1,
-          offsetY: 4
-        }, {
-          letter: 'n',
-          offsetX: 2,
-          offsetY: 4
-        }, {
-          letter: 'd',
-          offsetX: 3,
-          offsetY: 4
-        }], [{
-          letter: 's',
-          offsetX: 0,
-          offsetY: 4
-        }, {
-          letter: 't',
-          offsetX: 0,
-          offsetY: 3
-        }, {
-          letter: 'a',
-          offsetX: 0,
-          offsetY: 2
-        }, {
-          letter: 'r',
-          offsetX: 0,
-          offsetY: 1
-        }, {
-          letter: 't',
-          offsetX: 0,
-          offsetY: 0
-        }]
-      ]
+      }, {
+        letter: 'r',
+        offsetX: 0,
+        offsetY: 1
+      }, {
+        letter: 't',
+        offsetX: 0,
+        offsetY: 0
+      }]
     };
 
     // TODO position when the orientation changes
     $scope.positionBoard = function() {
       console.log("hello");
-      $scope.gameLetters;
+      var curLetter = $scope.game.gameLetters[0];
       $('#gameBoard').css({
-        'transform': 'translate(0px, ' + 50*5 + 'px)'
+        'transform': 'translate(' + curLetter.offsetX * -50 + 'px, ' + curLetter.offsetY * 50 + 'px)'
       });
     }
 
